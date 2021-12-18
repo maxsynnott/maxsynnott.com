494746d906261308c63d32eb94565315a62dff30
Max Synnott <maxryansynnott@gmail.com>
2021-12-18 18:23:52 +1300

    Moved links into own component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,49 +1,10 @@
 import { FC } from 'react'
-import { SocialLink } from './SocialLink'
-
-const links = [
-	{
-		text: 'instagram',
-		href: 'https://www.instagram.com/synnott.max',
-		brandClassName: 'instagram',
-		hoverColor: '#FFFFFF',
-		hoverBackgroundColor: '#f77737',
-	},
-	{
-		text: 'linkedin',
-		href: 'https://www.linkedin.com/in/maxsynnott/',
-		brandClassName: 'linkedin',
-		hoverColor: '#FFFFFF',
-		hoverBackgroundColor: '#2867b2',
-	},
-	{
-		text: 'github',
-		href: 'https://www.github.com/maxsynnott',
-		brandClassName: 'github',
-		hoverColor: '#FFFFFF',
-		hoverBackgroundColor: '#333',
-	},
-	{
-		text: 'email',
-		href: `mailto:maxryansynnott@gmail.com?subject=${encodeURIComponent(
-			'Please work for me!',
-		)}`,
-		brandClassName: 'email',
-		hoverColor: '#FFFFFF',
-		hoverBackgroundColor: '#111111',
-	},
-]
+import { SocialLinks } from './SocialLinks'
 
 export const Footer: FC = () => {
 	return (
-		<>
-			<div className="footer">
-				<div className="social-links-container">
-					{links.map((props, i) => (
-						<SocialLink key={i} {...props} />
-					))}
-				</div>
-			</div>
-		</>
+		<div className="footer">
+			<SocialLinks />
+		</div>
 	)
 }
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react'
+import { SocialLink } from './SocialLink'
+
+const LINKS = [
+	{
+		text: 'instagram',
+		href: 'https://www.instagram.com/synnott.max',
+		brandClassName: 'instagram',
+		hoverColor: '#FFFFFF',
+		hoverBackgroundColor: '#f77737',
+	},
+	{
+		text: 'linkedin',
+		href: 'https://www.linkedin.com/in/maxsynnott/',
+		brandClassName: 'linkedin',
+		hoverColor: '#FFFFFF',
+		hoverBackgroundColor: '#2867b2',
+	},
+	{
+		text: 'github',
+		href: 'https://www.github.com/maxsynnott',
+		brandClassName: 'github',
+		hoverColor: '#FFFFFF',
+		hoverBackgroundColor: '#333',
+	},
+	{
+		text: 'email',
+		href: `mailto:maxryansynnott@gmail.com?subject=${encodeURIComponent(
+			'Please work for me!',
+		)}`,
+		brandClassName: 'email',
+		hoverColor: '#FFFFFF',
+		hoverBackgroundColor: '#111111',
+	},
+]
+
+export const SocialLinks: FC = () => {
+	return (
+		<div className="social-links-container">
+			{LINKS.map((props, i) => (
+				<SocialLink key={i} {...props} />
+			))}
+		</div>
+	)
+}
