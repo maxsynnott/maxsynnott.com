9ceb60a76d44985a85e7e47c2eb1560a9687a163
Max Synnott <maxryansynnott@gmail.com>
2021-12-18 14:09:41 +1300

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import { FC } from "react";
+import { SocialLink } from "./components/SocialLink";
+
+const links = [
+	{ text: "instagram", href: "https://www.instagram.com/synnott.max" },
+	{ text: "linkedin", href: "https://www.linkedin.com/in/maxsynnott/" },
+	{ text: "github", href: "https://www.github.com/maxsynnott" },
+	{
+		text: "email",
+		href: `mailto:maxryansynnott@gmail.com?subject=${encodeURIComponent(
+			"Please work for me!"
+		)}`,
+	},
+];
 
 export const App: FC = () => {
-	return <div>Hello World!</div>;
+	return (
+		<>
+			<div className="header">
+				<h2 className="name">Max Synnott</h2>
+			</div>
+			<div className="content">
+				<h1 className="title full-width">Software Engineer</h1>
+			</div>
+			<div className="footer">
+				{links.map(({ text, href }) => (
+					<SocialLink text={text} href={href} />
+				))}
+			</div>
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+interface Props {
+	text: string;
+	href: string;
+}
+
+export const SocialLink: FC<Props> = ({ text, href }) => {
+	return (
+		<a className="social-link" href={href} target="_blank">
+			{text}
+		</a>
+	);
+};
